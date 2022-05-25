a96104d19dd6bc4c9643134136e8c803fb9db6aa
Leekunhee830 <gorrka159@naver.com>
2022-05-25 21:06:36 +0900

    ResultData 클래스 구현 후 getArticle에 적용

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.ArticleService;
+import com.example.demo.util.ResultData;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Article;
 
 
@@ -34,14 +36,14 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article=articleService.getArticle(id);
 		
-		if(article==null) {
-			return id+"번 게시물은 존재하지 않습니다.";
+		if(article==null) {	
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.",id ),article);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
