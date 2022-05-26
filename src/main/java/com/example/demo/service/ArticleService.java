cdf503d5050f642611797b0bfc9d36c90f1b11d5
Leekunhee830 <gorrka159@naver.com>
2022-05-26 20:56:00 +0900

    C:/Program Files/Git/usr/article/doAdd 에 ResultData 적용

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.example.demo.repository.ArticleRepository;
+import com.example.demo.util.ResultData;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Article;
 
 @Service
@@ -14,9 +16,11 @@ public class ArticleService {
 		this.articleRepository=articleRepository;
 	}
 	
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id= articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id),id);
 	}
 
 	public List<Article> getArticles() {
